b1a112fa7acc909338ee2dde55cc8b0143ae6d34
Samuel Vandamme <samuel@sava.be>
2018-04-19 15:14:56 +0200

    Updated version of script (currently has errors)

--- uptime/systemd.sh
+++ uptime/systemd.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Script to push status of enabled SystemD services to CoScale
-# Service value running 1, other 0
 
 for i in "$@"
 do
@@ -10,20 +9,25 @@ do
         # Configuration mode: return the custom metrics data should be defined
 
         echo -n '{';
-        echo -n '"maxruntime":1000,'
+        echo -n '"maxruntime":20000,'
         echo -n '"period": 60,'
 
         echo -n '"metrics":['
 
-        # Add services metrics definition
-        COUNTER=1
-        for SERVICE in `systemctl list-unit-files | grep enabled | awk '{print $1}'`; do
-            echo -n "{\"id\": ${COUNTER}, \"datatype\":\"DOUBLE\", \"name\":\"${SERVICE}\", \"description\":\"Status of ${SERVICE}\", \"groups\":\"SystemD\", \"unit\":\"\", \"tags\":\"\", \"calctype\":\"Instant\"},";
-            COUNTER=$((COUNTER+1))
-        done
+        echo -n '{ "id": 0, "name": "Disabled state", "description": "Service is in disabled state", "groups": "SystemD/State", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 1, "name": "Enabled state", "description": "Service is in enabled state", "groups": "SystemD/State", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 2, "name": "Indirect state", "description": "Service is in indirect state", "groups": "SystemD/State", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 3, "name": "Masked state", "description": "Service is in masked state", "groups": "SystemD/State", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 4, "name": "Static state", "description": "Services in in static state", "groups": "SystemD/State", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
 
-        # Push metric with total number of services enabled
-        echo -n '{"id":0, "datatype":"DOUBLE", "name":"Total number of enabled SystemD services", "description":"", "groups":"SystemD", "unit":"", "tags":"", "calctype":"Instant"}';
+        echo -n '{ "id": 5, "name": "Active substate", "description": "Service is in active substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 6, "name": "Elapsed substate", "description": "Service is in elapsed substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 7, "name": "Exited substate", "description": "Service is in exited substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 8, "name": "Listening substate", "description": "Service is in listening substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 9, "name": "Mounted substate", "description": "Services in in mounted substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 10, "name": "Running substate", "description": "Services in in running substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] },'
+        echo -n '{ "id": 11, "name": "Waiting substate", "description": "Services in in waiting substate", "groups": "SystemD/Substate", "unit": "", "tags": "", "calctype": "Instant", "dimensions": [ {"id": 1, "name":"Service"} ] }'
+
 
         echo ']}';
         ;;
@@ -31,16 +35,25 @@ do
         -d)
         # Data retrieval mode: return the data for the custom metrics
 
-        # Total number of services enabled
-        echo "M0 $(systemctl list-unit-files | grep enabled | wc -l)"
+        systemctl list-unit-files  --no-legend | while read line ; do
+            SERVICE=$(echo $line | awk '{print $1}')
+            STATE=$(echo $line | awk '{print $2}')
+            SUBSTATE=$(systemctl | grep "$SERVICE" 2> /dev/null | awk '{print $4}')
 
-        # Status per service
-        COUNTER=1
-        SERVICES=$(systemctl list-unit-files | grep enabled | awk '{print $1}')
-        for SERVICE in `systemctl list-unit-files | grep enabled | awk '{print $1}'`; do
-            echo "M${COUNTER} $(systemctl | grep ${SERVICE} | grep "running\|listening" | wc -l)"
-            COUNTER=$((COUNTER+1))
+            echo "M0 \"1:$SERVICE\" $(echo $STATE | grep disabled | wc -l)"
+            echo "M1 \"1:$SERVICE\" $(echo $STATE | grep enabled | wc -l)"
+            echo "M2 \"1:$SERVICE\" $(echo $STATE | grep indirect | wc -l)"
+            echo "M3 \"1:$SERVICE\" $(echo $STATE | grep masked | wc -l)"
+            echo "M4 \"1:$SERVICE\" $(echo $STATE | grep static | wc -l)"
+
+            echo "M5 \"1:$SERVICE\" $(echo $SUBSTATE | grep active | wc -l)"
+            echo "M6 \"1:$SERVICE\" $(echo $SUBSTATE | grep elapsed | wc -l)"
+            echo "M7 \"1:$SERVICE\" $(echo $SUBSTATE | grep exited | wc -l)"
+            echo "M8 \"1:$SERVICE\" $(echo $SUBSTATE | grep listening | wc -l)"
+            echo "M9 \"1:$SERVICE\" $(echo $SUBSTATE | grep mounted | wc -l)"
+            echo "M10 \"1:$SERVICE\" $(echo $SUBSTATE | grep running | wc -l)"
+            echo "M11 \"1:$SERVICE\" $(echo $SUBSTATE | grep waiting | wc -l)"
         done
-        ;;
+
     esac
 done
